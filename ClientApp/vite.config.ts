b608875e22aaa83025043a1e4b79437e6425c3c3
John Campion <john@brightshore.com>
2021-02-20 16:24:45 -0500

    Remove unused vite features

--- ClientApp/vite.config.ts
+++ ClientApp/vite.config.ts
@@ -5,10 +5,7 @@ import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
 import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 import ViteComponents from 'vite-plugin-components'
-import Markdown from 'vite-plugin-md'
 import { VitePWA } from 'vite-plugin-pwa'
-import VueI18n from '@intlify/vite-plugin-vue-i18n'
-import Prism from 'markdown-it-prism'
 
 export default defineConfig({
   server: {
@@ -32,24 +29,8 @@ export default defineConfig({
     // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
     Layouts(),
 
-    // https://github.com/antfu/vite-plugin-md
-    Markdown({
-      wrapperClasses: 'prose prose-sm m-auto text-left',
-      headEnabled: true,
-      markdownItSetup(md) {
-        // https://prismjs.com/
-        md.use(Prism)
-      },
-    }),
-
     // https://github.com/antfu/vite-plugin-components
     ViteComponents({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
-
-      // allow auto import and register components used in markdown
-      customLoaderMatcher: id => id.endsWith('.md'),
-
       // auto import icons
       customComponentResolvers: [
         // https://github.com/antfu/vite-plugin-icons
@@ -89,11 +70,6 @@ export default defineConfig({
           },
         ],
       },
-    }),
-
-    // https://github.com/intlify/vite-plugin-vue-i18n
-    VueI18n({
-      include: [path.resolve(__dirname, 'locales/**')],
     }),
   ],
   // https://github.com/antfu/vite-ssg
